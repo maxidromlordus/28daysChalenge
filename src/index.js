0aeb933d0b2650a77d35679e77881d22f4aab129
Maxim <maxidromlordus@gmail.com>
2021-04-30 11:56:59 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// elements
+
+const wrapper = document.getElementById("wrapper");
+
+//create 28 fields
+
+for (let i = 0; i <= 28; i++) {
+  const field = document.createElement("div");
+  field.id = `field${i}`;
+  field.classList.add("field");
+  field.innerHTML = `${i}`;
+  wrapper.appendChild(field);
+}
+
+
