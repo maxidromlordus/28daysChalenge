5c4a70f0ffb105764406f5ced80addccb2e48513
Maxim <maxidromlordus@gmail.com>
2021-04-30 15:56:25 +0300

    html changes done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-// elements
-
 const wrapper = document.getElementById("wrapper");
 
 //create 28 fields
 
-for (let i = 0; i <= 28; i++) {
+for (let i = 1; i <= 28; i++) {
   const field = document.createElement("div");
   field.id = `field${i}`;
   field.classList.add("field");
   field.innerHTML = `${i}`;
   wrapper.appendChild(field);
 }
-
-
