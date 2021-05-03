58f9270d4f37ea3b3851fb74c5c96ccc0ed20704
Maxim Samohvalov <maxidromlordus@gmail.com>
2021-05-03 17:56:11 +0300

    promise add array

--- dist/index.js
+++ dist/index.js
@@ -2,46 +2,70 @@
 
 var _localforage = require("localforage");
 
-var wrapper = document.getElementById("wrapper"); // Array for save in localstorage
-//
-//create 28 fields/
-// Check in localstorage fields status
+var wrapper = document.getElementById("wrapper"); //create 28 fields/
+// createGrid function. Get array from localStorage - if not exists creates new one
 
 function createGrid(arr) {
-  for (var i = 1; i < arr.length; i++) {
-    var field = document.createElement("div");
-    field.id = "field".concat(i);
-    field.classList.add("field");
-    field.classList.add("neutral");
-    field.innerHTML = "".concat(i);
-    wrapper.appendChild(field);
-  }
-}
+  return new Promise(function (resolve, reject) {
+    for (var i = 1; i < arr.length; i++) {
+      var field = document.createElement("div");
+      field.id = "field".concat(i);
 
-(0, _localforage.getItem)("28days", function (err, value) {
-  console.log(value);
+      switch (arr[i]) {
+        case 0:
+          field.classList.add("neutral");
 
-  if (value === null) {
-    var arr = [];
+        case 1:
+          field.classList.add("done");
 
-    for (var i = 1; i <= 28; i++) {
-      arr[i] = 0;
+        case 2:
+          field.classList.add("fail");
+      }
+
+      field.classList.add("field");
+      field.innerHTML = "".concat(i);
+      wrapper.appendChild(field);
     }
 
-    createGrid(arr);
+    resolve(arr);
+  });
+}
+
+function changeArrayData(arr) {
+  wrapper.addEventListener("click", function (e) {
+    if (e.target.classList.contains("neutral")) {
+      e.target.classList.remove("neutral");
+      e.target.classList.add("done");
+      arr[parseInt(e.target.innerText)] = 1;
+      console.log(arr[parseInt(e.target.innerText)]);
+    } else if (e.target.classList.contains("done")) {
+      e.target.classList.remove("done");
+      e.target.classList.add("fail");
+      arr[parseInt(e.target.innerText)] = 2;
+      console.log(arr[parseInt(e.target.innerText)]);
+    } else if (e.target.classList.contains("fail")) {
+      e.target.classList.remove("fail");
+      e.target.classList.add("neutral");
+      arr[parseInt(e.target.innerText)] = 0;
+      console.log(arr[parseInt(e.target.innerText)]);
+    }
+  });
+}
+
+(0, _localforage.getItem)("28days", function (err, arr) {
+  if (arr === null) {
+    var _arr = [];
+
+    for (var i = 1; i <= 28; i++) {
+      _arr[i] = 0;
+    }
+
+    createGrid(_arr).then(function (result) {
+      changeArrayData(result);
+    });
   } else {
-    createGrid(value);
+    createGrid(arr);
   }
-});
-wrapper.addEventListener("click", function (e) {
-  if (e.target.classList.contains("neutral")) {
-    e.target.classList.remove("neutral");
-    e.target.classList.add("done");
-  } else if (e.target.classList.contains("done")) {
-    e.target.classList.remove("done");
-    e.target.classList.add("fail");
-  } else if (e.target.classList.contains("fail")) {
-    e.target.classList.remove("fail");
-    e.target.classList.add("neutral");
-  }
+
+  console.log(arr); //changeArrayData(arr);
 });
--- src/index.js
+++ src/index.js
@@ -2,46 +2,64 @@ import { getItem } from "localforage";
 const wrapper = document.getElementById("wrapper");
 
 
-// Array for save in localstorage
-//
+
 //create 28 fields/
-// Check in localstorage fields status
+// createGrid function. Get array from localStorage - if not exists creates new one
 function createGrid(arr) {
-  for( let i = 1; i < arr.length; i++) {
-    const field = document.createElement("div");
-    field.id = `field${i}`;
-    field.classList.add("field");
-    field.classList.add("neutral");
-    field.innerHTML = `${i}`;
-    wrapper.appendChild(field);
-  }
-  
+  return new Promise((resolve, reject) => {
+    for( let i = 1; i < arr.length; i++) {
+      const field = document.createElement("div");
+      field.id = `field${i}`;
+      switch(arr[i]) {
+        case 0: field.classList.add("neutral");
+        case 1: field.classList.add("done");
+        case 2: field.classList.add("fail");
+      }
+      field.classList.add("field");
+      field.innerHTML = `${i}`;
+      wrapper.appendChild(field);
+    }
+    resolve(arr);
+  })
 }
 
-getItem("28days", function (err, value) {
-  console.log(value);
-  if(value === null) {
+function changeArrayData(arr) {
+  wrapper.addEventListener("click", (e) => {
+    if (e.target.classList.contains("neutral")) {
+      e.target.classList.remove("neutral");
+      e.target.classList.add("done");
+      arr[parseInt(e.target.innerText)] = 1;
+      console.log(arr[parseInt(e.target.innerText)]);
+    } else if (e.target.classList.contains("done")) {
+      e.target.classList.remove("done");
+      e.target.classList.add("fail");
+      arr[parseInt(e.target.innerText)] = 2;
+      console.log(arr[parseInt(e.target.innerText)]);
+    } else if (e.target.classList.contains("fail")) {
+      e.target.classList.remove("fail");
+      e.target.classList.add("neutral");
+      arr[parseInt(e.target.innerText)] = 0;
+      console.log(arr[parseInt(e.target.innerText)]);
+    }
+  });
+}
+getItem("28days", function (err, arr) {
+  if(arr === null) {
     const arr = [];
     for (let i = 1; i <= 28; i++) {
       arr[i] = 0;
     }
-    createGrid(arr);
+    createGrid(arr).then((result) => {
+      changeArrayData(result);
+    });
   } else {
-    createGrid(value);
+    createGrid(arr);
   }
+  console.log(arr);
+  //changeArrayData(arr);
+  
 });
 
 
 
-wrapper.addEventListener("click", (e) => {
-  if (e.target.classList.contains("neutral")) {
-    e.target.classList.remove("neutral");
-    e.target.classList.add("done");
-  } else if (e.target.classList.contains("done")) {
-    e.target.classList.remove("done");
-    e.target.classList.add("fail");
-  } else if (e.target.classList.contains("fail")) {
-    e.target.classList.remove("fail");
-    e.target.classList.add("neutral");
-  }
-});
+
